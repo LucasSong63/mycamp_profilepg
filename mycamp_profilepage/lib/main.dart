527e3e3a60d27b5c70098743b7b2a366a2f6c4c1
Lucas Song <88481698+LucasSong63@users.noreply.github.com>
2023-05-02 20:58:50 +0800

    ProfileMenu widget and Edit Profile Page

--- mycamp_profilepage/lib/main.dart
+++ mycamp_profilepage/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mycamp_profilepage/update_profile';
 
 void main() => runApp(MaterialApp(
       title: 'Profile',
@@ -25,43 +26,141 @@ class ProfilePage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Container(
           padding: const EdgeInsets.all(8.0),
-          child: Center(
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 20,
-                ),
-                SizedBox(
-                  width: 150,
-                  height: 150,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(150),
-                    child: Image(
-                      image: AssetImage('assets/yanliu.png'),
-                    ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SpacerH10(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ProfilePhoto_Widget(),
+                  Column(
+                    children: [
+                      UsernameTxt(),
+                      PhoneNoTxt(),
+                    ],
                   ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  'Username',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-                ),
-                Text(
-                  'Phone Number',
-                  style: TextStyle(fontSize: 12),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Text('My Account'),
-              ],
-            ),
+                ],
+              ),
+              SpacerH30(),
+              Divider(),
+              SpacerH30(),
+              Text(
+                '\t\t\tMy Account',
+                style: TextStyle(fontSize: 18),
+              ),
+              SpacerH30(),
+              Profile_widget(
+                  title: "Edit Profile",
+                  icon: Icons.edit_note_outlined,
+                  iconsize: 35,
+                  onPress: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => UpdateProfile(),
+                      ),
+                    );
+                  }),
+              Profile_widget(
+                title: "Settings",
+                icon: Icons.settings,
+                iconsize: 28,
+                onPress: () {},
+              ),
+            ],
           ),
-
-          //Text('My Account')
         ),
+      ),
+    );
+  }
+
+  SizedBox SpacerH30() {
+    return SizedBox(
+      height: 30,
+    );
+  }
+
+  Text PhoneNoTxt() {
+    return Text(
+      'Phone Number',
+      style: TextStyle(fontSize: 12),
+    );
+  }
+
+  Text UsernameTxt() {
+    return Text(
+      'Username',
+      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+      maxLines: 2,
+    );
+  }
+
+  SizedBox ProfilePhoto_Widget() {
+    return SizedBox(
+      width: 150,
+      height: 150,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(150),
+        child: Image(
+          fit: BoxFit.cover,
+          image: AssetImage('assets/yanliu.png'),
+        ),
+      ),
+    );
+  }
+
+  SizedBox SpacerH10() {
+    return SizedBox(
+      height: 10,
+    );
+  }
+
+  SizedBox SpacerW50() {
+    return SizedBox(
+      width: 50,
+    );
+  }
+}
+
+class Profile_widget extends StatelessWidget {
+  const Profile_widget({
+    Key? key,
+    required this.title,
+    required this.icon,
+    required this.iconsize,
+    required this.onPress,
+    // this.endIcon = true,
+  }) : super(key: key);
+  final String title;
+  final IconData icon;
+  final double iconsize;
+  final VoidCallback onPress;
+  // final bool endIcon;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: onPress,
+      leading: Container(
+        width: 45,
+        height: 45,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.blue.withOpacity(0.4)),
+        child: Icon(
+          icon,
+          size: iconsize,
+        ),
+      ),
+      title: Text(title),
+      trailing: Container(
+        width: 30,
+        height: 30,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(100),
+            color: Colors.grey.withOpacity(0.1)),
+        child: Icon(Icons.chevron_right_rounded),
       ),
     );
   }
